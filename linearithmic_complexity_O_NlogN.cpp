2e7ad5e179ec0f3cfeb180f2fb65b5e8b1dfb96c
MashrafiGit <mdmasrafi902@gmail.com>
2025-06-24 05:09:39 +0600

    linearithmic complexity O(NlogN)

---
+++ linearithmic_complexity_O_NlogN.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    int n, m;
+    cin >> n >> m;
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int i = 1; i <= m; i*=2)
+        {
+            cout << "Hello" << endl;
+        }
+    }
+
+    return 0;
+}
